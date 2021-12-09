e6dce3140089f5723d3785b7976a47a226ad7cbf
SaniSaiti <sani.saiti@bluewin.ch>
2021-12-09 15:21:57 +0100

    BASIC PRINT

---
+++ models/movable-objet.class.js
@@ -0,0 +1,28 @@
+class MovableObject {
+    x = 120;
+    y = 250;
+    img;
+
+    height = 150;
+    width = 100;
+
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+
+
+    }
+
+
+
+    moveRight() {
+
+
+        console.log('Move-Rigth', this.x);
+    }
+
+    moveLeft() {
+
+    }
+
+}
